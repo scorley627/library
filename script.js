e03016cd1ffcfba58278fe6bf252ccda1073fecc
Sean Corley <200403627+scorley627@users.noreply.github.com>
2025-05-30 15:05:49 -0600

    Create dialog for adding books to library

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const library = [];
 
+function Book(id, title, author, numPages, haveRead) {
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  this.haveRead = haveRead;
+}
+
+Book.prototype.getSurname = function () {
+  const author = this.author.split(" ");
+  let surname = author.at(-1);
+  if (surname.toLowerCase().includes("jr")) {
+    surname = author.at(-2);
+  }
+  return surname;
+};
+
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 366, true);
 addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
 addBookToLibrary("Anna Karenina", "Leo Tolstoy", 964, false);
@@ -10,6 +27,7 @@ addBookToLibrary("The Grapes of Wrath", "John Steinbeck", 496, false);
 addBookToLibrary("The Adventures of Huckleberry Finn", "Mark Twain", 327, true);
 
 document.addEventListener("DOMContentLoaded", displayLibrary);
+document.addEventListener("click", handleClick);
 
 function addBookToLibrary(title, author, numPages, haveRead) {
   const newId = crypto.randomUUID();
@@ -17,9 +35,9 @@ function addBookToLibrary(title, author, numPages, haveRead) {
 
   let i = 0;
   let added = false;
-  const newSurname = newBook.author.split(" ").at(-1);
+  const newSurname = newBook.getSurname();
   while (i < library.length && !added) {
-    const currentSurname = library[i].author.split(" ").at(-1);
+    const currentSurname = library[i].getSurname();
     if (newSurname.localeCompare(currentSurname) == -1) {
       library.splice(i, 0, newBook);
       added = true;
@@ -33,16 +51,9 @@ function addBookToLibrary(title, author, numPages, haveRead) {
   }
 }
 
-function Book(id, title, author, numPages, haveRead) {
-  this.id = id;
-  this.title = title;
-  this.author = author;
-  this.numPages = numPages;
-  this.haveRead = haveRead;
-}
-
 function displayLibrary() {
   const bookCardArea = document.querySelector(".book-card-area");
+  bookCardArea.replaceChildren();
   for (const book of library) {
     const card = document.createElement("div");
     const title = document.createElement("h1");
@@ -61,4 +72,54 @@ function displayLibrary() {
     card.appendChild(numPages);
     bookCardArea.appendChild(card);
   }
+}
+
+function handleClick(event) {
+  const isNewBookButton = event.target.className == "new-book-button";
+  const isDialogCloseButton =
+    event.target.className == "new-book-dialog__close-button";
+  const isDialogSubmitButton =
+    event.target ==
+    document.querySelector(".new-book-form button[type=submit]");
+
+  if (isNewBookButton) {
+    showNewBookDialog();
+  } else if (isDialogCloseButton) {
+    closeNewBookDialog();
+  } else if (isDialogSubmitButton) {
+    event.preventDefault();
+    submitNewBook();
+  }
+}
+
+function showNewBookDialog() {
+  const bookDialog = document.querySelector(".new-book-dialog");
+  bookDialog.showModal();
+}
+
+function closeNewBookDialog() {
+  const bookDialog = document.querySelector(".new-book-dialog");
+  const bookForm = document.querySelector(".new-book-form");
+  bookDialog.close();
+  bookForm.reset();
+}
+
+function submitNewBook() {
+  const titleField = document.getElementById("title");
+  const authorField = document.getElementById("author");
+  const numPagesField = document.getElementById("num_pages");
+  const haveReadField = document.getElementById("have_read");
+
+  const title = titleField.value;
+  const author = authorField.value;
+  const numPages = Number(numPagesField.value);
+  const haveRead = haveReadField.checked;
+
+  const bookDialog = document.querySelector(".new-book-dialog");
+  const bookForm = document.querySelector(".new-book-form");
+  bookDialog.close();
+  bookForm.reset();
+
+  addBookToLibrary(title, author, numPages, haveRead);
+  displayLibrary();
 }
